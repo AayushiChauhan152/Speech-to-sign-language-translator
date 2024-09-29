54f896fb7ddf402fd6de29877246fb134669d216
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2024-09-29 14:22:07 +0530

    Update settings.py

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '3k7=!d39#4@_&5a6to&4=_=j(c^v0(vv91cj5+9e8+d4&+01jb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
